4e9f907bdb6b74c511b47206ab095d64f1941f17
craigmarc <cmarcinkiewicz2000@yahoo.com>
2025-07-11 07:48:27 -0400

    started new repo transfering suduko api to local repo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
@@ -25,12 +25,13 @@ app.use(
 
 app.use(cors());
 
-const mongoDB = process.env.MONGODB_URI 
+// no database used
+/*const mongoDB = process.env.MONGODB_URI 
 
 main().catch((err) => console.log(err));
 async function main() {
 await mongoose.connect(mongoDB);
-}
+}*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
